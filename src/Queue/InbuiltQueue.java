f651e5ed1d030c586b00d67aaa03150cf17ae7b3
rahil <rahilganatra@gmail.com>
2024-01-05 19:11:33 +0530

    Stack C1

---
+++ src/Queue/InbuiltQueue.java
@@ -0,0 +1,22 @@
+package Queue;
+
+import java.util.*;
+
+public class InbuiltQueue {
+
+
+
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(3);
+        queue.add(6);
+        queue.add(5);
+
+        System.out.println(queue.peek());
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
+
+        Deque<Integer> deque = new ArrayDeque<>();
+
+    }
+}
--- src/Stack/InBuiltStack.java
+++ src/Stack/InBuiltStack.java
@@ -1,8 +1,17 @@
 package Stack;
 
+import java.util.Stack;
+
 public class InBuiltStack {
 
     public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(34);
+        stack.push(45);
+        stack.push(2);
 
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
     }
 }
