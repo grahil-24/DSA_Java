ec54c2c3b1426ba6a43f2e891d8cad9729223522
grahil-24 <rahilganatra@gmail.com>
2024-07-17 10:21:10 +0530

    memoization

--- src/TwoDimDP/LongestCommonSubsequence.java
+++ src/TwoDimDP/LongestCommonSubsequence.java
@@ -3,20 +3,37 @@ package TwoDimDP;
 public class LongestCommonSubsequence {
 
     public int longestCommonSubsequence(String text1, String text2) {
-        return helper(text1, text2, 0, 0);
+        int[][] cache = new int[text1.length()][text2.length()];
+        for(int i = 0; i < text1.length(); i++){
+            for(int j = 0; j < text2.length(); j++){
+                cache[i][j] = -1;
+            }
+        }
+
+        return helper(text1, text2, 0, 0, cache);
     }
 
-    public int helper(String text1, String text2, int index1, int index2) {
-        if(index1 == text1.length() || index2 == text2.length()){
+    public int helper(String text1, String text2, int index1, int index2, int[][] cache) {
+        if (index1 == text1.length() || index2 == text2.length()) {
             return 0;
         }
 
-        if(text1.charAt(index1) == text2.charAt(index2)){
-            return 1 + helper(text1, text2, index1 + 1, index2 + 1);
-        }else{
-            return Math.max(helper(text1, text2, index1 + 1, index2), helper(text1, text2, index1, index2 + 1));
+        if (cache[index1][index2] != -1) {
+            return cache[index1][index2];
         }
+
+        if (text1.charAt(index1) == text2.charAt(index2)) {
+            cache[index1][index2] = 1 + helper(text1, text2, index1 + 1, index2 + 1, cache);
+        } else {
+            cache[index1][index2] =  Math.max(
+                helper(text1, text2, index1 + 1, index2, cache),
+                helper(text1, text2, index1, index2 + 1, cache)
+            );
+        }
+        return cache[index1][index2];
     }
+
+
 
     public static void main(String[] args) {
         LongestCommonSubsequence lcs = new LongestCommonSubsequence();
