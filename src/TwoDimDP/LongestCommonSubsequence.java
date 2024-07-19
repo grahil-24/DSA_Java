eab1bf05e09cefa1f0b18250dd2d6dac8ca6ee58
grahil-24 <rahilganatra@gmail.com>
2024-07-19 09:03:59 +0530

    lcs top down, 2d DP

--- src/TwoDimDP/LongestCommonSubsequence.java
+++ src/TwoDimDP/LongestCommonSubsequence.java
@@ -3,6 +3,20 @@ package TwoDimDP;
 public class LongestCommonSubsequence {
 
     public int longestCommonSubsequence(String text1, String text2) {
+        int str1_len  = text1.length();
+        int str2_len = text2.length();
+        int[][] dp = new int[str1_len + 1][str2_len + 1];
+        for(int i = 1; i <= str1_len; i++) {
+            for(int j = 1; j <= str2_len; j++) {
+                if(text1.charAt(i) == text2.charAt(j)) {
+                    dp[i+1][j+1] = dp[i][j] + 1;
+                }else{
+                    dp[i+1][j+1] = Math.max(dp[i+1][j], dp[i][j+1]);
+                }
+            }
+        }
+        return dp[str1_len][str2_len];
+
         int[][] cache = new int[text1.length()][text2.length()];
         for(int i = 0; i < text1.length(); i++){
             for(int j = 0; j < text2.length(); j++){
